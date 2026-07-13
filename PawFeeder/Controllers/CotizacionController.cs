f54b1f030342828cf706c35333f535cdc9aa872e
Jaqueline <ijaquelinegagon@gmail.com>
2026-07-13 11:53:01 -0600

    Agrega envío de cotizaciones por correo

---
+++ PawFeeder/Controllers/CotizacionController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PawFeeder.Models;
+using PawFeeder.Services;
+
+namespace PawFeeder.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CotizacionController : ControllerBase
+    {
+        private readonly EmailService _emailService;
+
+        public CotizacionController(EmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost("enviar")]
+        public async Task<IActionResult> EnviarCotizacion([FromBody] CotizacionRequest request)
+        {
+            string mensaje = $@"
+                <h2>Cotización PawFeeder</h2>
+
+                <p>Gracias por solicitar una cotización.</p>
+
+                <table style='border-collapse:collapse;'>
+
+                    <tr>
+                        <td><b>Contenedor:</b></td>
+                        <td>{request.Contenedor}</td>
+                    </tr>
+
+                    <tr>
+                        <td><b>Material:</b></td>
+                        <td>{request.Material}</td>
+                    </tr>
+
+                    <tr>
+                        <td><b>Cantidad:</b></td>
+                        <td>{request.Cantidad}</td>
+                    </tr>
+
+                    <tr>
+                        <td><b>Total:</b></td>
+                        <td>$ {request.Total} MXN</td>
+                    </tr>
+
+                </table>
+
+                <br>
+
+                <p>Gracias por confiar en <b>PawFeeder</b>.</p>
+            ";
+
+            await _emailService.EnviarCorreoAsync(
+                request.Correo,
+                "Cotización PawFeeder",
+                mensaje);
+
+            return Ok(new
+            {
+                mensaje = "Correo enviado correctamente."
+            });
+        }
+    }
+}
